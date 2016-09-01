f8f89bd9cc3357d8333baf644003cd44c4503076
Marwan Zogheib <mzogheib@gmail.com>
2016-09-01 18:49:18 +1000

    Add messaging for watch

---
+++ src/c/messaging/messaging.c
@@ -0,0 +1,30 @@
+#include "messaging.h"
+#include "../windows/dialog_config_window.h"
+
+void messaging_init() {
+    // Register message callbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+
+    // Open sesame
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Message received!");
+}
+
+void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed! %d %d %d", reason, APP_MSG_SEND_TIMEOUT, APP_MSG_SEND_REJECTED);
+
+}
+
+void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
---
+++ src/c/messaging/messaging.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <pebble.h>
+
+void messaging_init();
+void inbox_received_callback(DictionaryIterator *iterator, void *context);
+void inbox_dropped_callback(AppMessageResult reason, void *context);
+void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context);
+void outbox_sent_callback(DictionaryIterator *iterator, void *context);
