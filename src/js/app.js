429a40f35c15a0f3adbd9557e48299a9f64db45e
Marwan Zogheib <mzogheib@gmail.com>
2016-09-09 00:00:15 +1000

    ISSUE #5 calculate the date range for any start day

--- src/js/app.js
+++ src/js/app.js
@@ -51,44 +51,90 @@ Pebble.addEventListener("ready", function(e) {
 });
 
 function getFinancialReport() {
-    // 1. Get the financial date range.
-    // 2. Get the sums for this range
-    // 3. Send to the watch
-
     var deferred = Q.defer();
 
     toshl.get('me')
-        .then(function (response) {
-            // TODO: hard coded for now. Calculate this dynamically
-            // var start_day = response['start_day'];
-
-            var from_date = '2016-09-01';
-            var to_date = '2016-09-30';
-
-            var params = {
-                from: from_date,
-                to: to_date
-            };
+        .then(function (response){
+            // Get the financial date range
+            var params = getDateRange(response['start_day']);
 
             return toshl.get('me/summary', params);
         })
         .then(function (response) {
-            var expenses_planned = response['expenses']['sum_planned'];
-            var expenses_total = response['expenses']['sum'];
-            var expenses_actual = expenses_total - expenses_planned;
+            // Get the financial report for this range
+            var expensesPlanned = response['expenses']['sum_planned'];
+            var expensesTotal = response['expenses']['sum'];
+            var expensesActual = expensesTotal - expensesPlanned;
             var from = new Date(response['from']).toString().substring(4, 10);
             var to = new Date(response['to']).toString().substring(4, 10);
-            var expenses_header = from + ' - ' + to;
+            var expensesHeader = from + ' - ' + to;
 
             var report = {
-                'EXPENSES_HEADER': expenses_header,
-                'EXPENSES_ACTUAL': expenses_actual.toFixed(2),
-                'EXPENSES_PLANNED': expenses_planned.toFixed(2),
-                'EXPENSES_TOTAL': expenses_total.toFixed(2)
+                'EXPENSES_HEADER': expensesHeader,
+                'EXPENSES_ACTUAL': expensesActual.toFixed(2),
+                'EXPENSES_PLANNED': expensesPlanned.toFixed(2),
+                'EXPENSES_TOTAL': expensesTotal.toFixed(2)
             };
 
             deferred.resolve(report);
         });
 
     return deferred.promise;
+
+    function getDateRange(startDay) {
+        // TODO: this is a bit of a nightmare but it works. Try to simplify it.
+        var today = new Date();
+        var endOfThisMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+        var fromDate;
+        var toDate;
+        var endDay;
+
+        if(startDay === 1) {
+            endDay = endOfThisMonth.getDate();
+            fromDate = new Date(today.getFullYear(), today.getMonth(), startDay);
+            toDate = new Date(today.getFullYear(), today.getMonth(), endDay);
+        } else {
+            endDay = startDay - 1;
+
+            var startOfThisMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+
+            var endOfLastMonth = new Date();
+            endOfLastMonth.setDate(startOfThisMonth.getDate() - 1)
+
+            var startOfNextMonth = new Date();
+            startOfNextMonth.setDate(endOfThisMonth.getDate() + 1)
+
+            var thisMonth = {
+                startDate: new Date(today.getFullYear(), today.getMonth(), startDay),
+                endDate: new Date(today.getFullYear(), today.getMonth(), endDay)
+            };
+            thisMonth.endDate = Math.min(thisMonth.endDate, endOfThisMonth)
+
+            var lastMonth = {
+                startDate: new Date(endOfLastMonth.getFullYear(), endOfLastMonth.getMonth(), startDay)
+            };
+            lastMonth.startDate = Math.min(lastMonth.startDate, startOfThisMonth)
+
+            var nextMonth = {
+                endDate: new Date(startOfNextMonth.getFullYear(), startOfNextMonth.getMonth(), endDay)
+            };
+
+            fromDate = new Date((today >= lastMonth.startDate && today <= thisMonth.endDate) ? lastMonth.startDate : thisMonth.startDate);
+            toDate = new Date((today >= thisMonth.startDate && today <= nextMonth.endDate) ? nextMonth.endDate : thisMonth.endDate);
+        }
+
+        function formatDate(date) {
+            // Source: http://stackoverflow.com/questions/10830357/javascript-toisostring-ignores-timezone-offset
+            var tzoffset = date.getTimezoneOffset() * 60000;
+            var localISOTime = (new Date(date - tzoffset)).toISOString().slice(0,-1);
+
+            return localISOTime.substring(0, 10);
+        }
+
+        return {
+            'from': formatDate(fromDate),
+            'to': formatDate(toDate)
+        }
+    }
 }
